ef82c3df36cb83cc8ddac33e6e55cf2d3aa13075
Nidhi <the.nidhi.bhat@gmail.com>
2026-08-26 22:36:05 +0530

    fix: ensure frontend connects to all local emulators during development

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,8 +1,8 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, signInAnonymously } from "firebase/auth";
-import { getStorage } from "firebase/storage";
+import { getAuth, signInAnonymously, connectAuthEmulator } from "firebase/auth";
+import { getStorage, connectStorageEmulator } from "firebase/storage";
 import { getFunctions, connectFunctionsEmulator } from "firebase/functions";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -18,8 +18,10 @@ export const auth = getAuth(app);
 export const storage = getStorage(app);
 export const functions = getFunctions(app);
 export const db = getFirestore(app);
-
 // Use local emulators in development if needed
 if (import.meta.env.DEV) {
-  // connectFunctionsEmulator(functions, "127.0.0.1", 5001);
+  connectFunctionsEmulator(functions, "127.0.0.1", 5001);
+  connectAuthEmulator(auth, "http://127.0.0.1:9099");
+  connectStorageEmulator(storage, "127.0.0.1", 9199);
+  connectFirestoreEmulator(db, "127.0.0.1", 8080);
 }
